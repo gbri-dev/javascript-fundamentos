dbb7b8c0f659072416d83ef6c1fd43fc30985d4d
Gabriel C Silva <gabriel.camposdasilva@hotmail.com>
2022-10-22 14:32:35 -0300

    funcao versao2

--- scripts/funcoes.js
+++ scripts/funcoes.js
@@ -10,11 +10,22 @@ function calcularMedia(notas) {
 
     return media
 }
-function aprovacao(media) {
-    let result = media >= 7 ? "aprovado" : "reprovado"
-    return result
+//Primeira versão
+// function aprovacao(media) {
+//     let result = media >= 7 ? "aprovado" : "reprovado"
+//     return result
+// }
+
+// console.log("Média: " + calcularMedia([8, 10, 6, 7]))
+// console.log(aprovacao(calcularMedia([8, 10, 6, 7])))
+
+//Segunda versão
+function aprovacao(notas) {
+    let media = calcularMedia(notas)
+    let condicao = media >= 8 ? "Aprovado!" : "Reprovado!"
+    return 'Média: ' + media + ' - Resultado: ' + condicao
 }
 
-console.log("Média: " + calcularMedia([8, 10, 6, 7]))
-console.log(aprovacao(calcularMedia([8, 10, 6, 7])))
-
+console.log(aprovacao([8, 8, 6, 10]))
+console.log(aprovacao([9, 7, 8, 10]))
+console.log(aprovacao([7, 8, 6, 10]))
