b5eb18bc092f2bc9c6d2fd96f8d4b29e5b917cf8
Gabriel C Silva <46769816+gbri-dev@users.noreply.github.com>
2023-12-20 16:32:34 -0300

    validação

--- src/js/script.js
+++ src/js/script.js
@@ -13,10 +13,15 @@
 // JQUERY
 
 $(document).ready(function(){
+    $('#cep').mask('00000-000')
+    
     $('#btn-buscar-cep').on('click', function(){
         const xhttp = new XMLHttpRequest()
         const cep = $('#cep').val()
         const endpoint = `https://viacep.com.br/ws/${cep}/json`
+        const botao = $(this)
+        $(botao).find('i').addClass('d-none')
+        $(botao).find('span').removeClass('d-none')
 
         $.ajax(endpoint).done(function(resposta){
             let logradouro = ""            
@@ -31,6 +36,11 @@ $(document).ready(function(){
             }
             const endereco = `${logradouro}${bairro}${cidade} - ${estado}`
             $('#endereco').val(endereco)
+
+            setTimeout(function(){
+                $(botao).find('i').removeClass('d-none')
+                $(botao).find('span').addClass('d-none')
+            }, 1000)
         })
     })
 })
