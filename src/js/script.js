3942e20fb309967fd6fd754a598346237383a5d3
Gabriel C Silva <46769816+gbri-dev@users.noreply.github.com>
2023-12-21 14:12:15 -0300

    exercicio_mdl23

--- src/js/script.js
+++ src/js/script.js
@@ -1,95 +1,21 @@
-// JAVASCRIPT
-// document.addEventListener('DOMContentLoaded', function(){
-//     document.getElementById('btn-buscar-cep').addEventListener('click', function(){
-//         const xhttp = new XMLHttpRequest()
-//         const cep = document.getElementById('cep').value
-//         const endpoint = `https://viacep.com.br/ws/${cep}/json`
+document.addEventListener('DOMContentLoaded', function(){
+    const nameElement = document.querySelector('#name')
+    const userNameElement = document.querySelector('#username')
+    const avatarElement = document.querySelector('#avatar')
+    const reposElement = document.querySelector('#repos')
+    const followersElement = document.querySelector('#followers')
+    const followingElement = document.querySelector('#following')
+    const linkElement = document.querySelector('#link')
 
-//         xhttp.open('GET', endpoint)
-//         xhttp.send()
-//     })
-// })
-
-// JQUERY
-const jqueryAjax = (endpoint, botao) => {
-    $.ajax(endpoint).done(function(objetoResposta){
-        corpoDaRequisicao(objetoResposta)
-        spinnerCep(botao)
-    })
-}
-
-//requisições via api usando FETCH
-const requisicaoFetch = (endpoint, botao) => {
-    fetch(endpoint).then(function(resposta){
-        return resposta.json()
+    fetch('https://api.github.com/users/gbri-dev').then(function(res){
+        return res.json()
     }).then(function(json){
-        corpoDaRequisicao(json, botao)
-    }).catch(function(erro){
-        alert("Ocorreu um erro ao buscar o endereço, tente novamente mais tarde. \n\nErro: " + erro)               
-    }).finally(function(){
-        spinnerCep(botao)
-    })
-}
-
-$(document).ready(function(){
-    //JQUERY MASK PLUGIN
-    $('#cep').mask('00000-000')
-    
-    $('#btn-buscar-cep').on('click', function(){        
-        const cep = $('#cep').val()
-        const endpoint = `https://viacep.com.br/ws/${cep}/json`
-        const botao = $(this)
-        $(botao).find('i').addClass('d-none')
-        $(botao).find('span').removeClass('d-none')
-        //jqueryAjax(endpoint, botao)
-        requisicaoFetch(endpoint, botao)        
-    })
-
-    $('#formulario-pedido').submit(function(e){
-        e.preventDefault()
-        const primeiroNome = $('#nome').val().length
-        if( primeiroNome < 3){
-            $('#errorNome').text("O nome precisa conter no mínimo 3 caracteres.")
-            $('#errorNome').removeClass('d-none')
-            throw new Error('O nome precisa conter no mínimo 3 caracteres.')
-        }
-
-        $('#errorNome').addClass('d-none')        
-        cleanForm()
-        console.info("success")
+        nameElement.innerHTML = json.name
+        userNameElement.innerHTML = json.login
+        avatarElement.src = json.avatar_url
+        reposElement.innerHTML = json.public_repos
+        followersElement.innerHTML = json.followers
+        followingElement.innerHTML = json.following
+        linkElement.href = json.html_url
     })
 })
-
-const cleanForm = () => {
-    $('#formulario-pedido input').val("")
-}
-
-const corpoDaRequisicao = (objetoResposta) => {
-    let logradouro = ""            
-    let bairro = ""
-    const cidade = objetoResposta.localidade
-    const estado = objetoResposta.uf
-    let endereco = ""
-    if(objetoResposta.logradouro !== "" && objetoResposta.logradouro !== undefined){
-       logradouro = objetoResposta.logradouro + ", "
-    }
-    if(objetoResposta.bairro !== "" && objetoResposta.bairro !== undefined){
-       bairro = objetoResposta.bairro + " - "
-    }
-    if(cidade == undefined || estado == undefined){
-        $('#errorCep').text("Não foi encontrador nenhum endereço, tente outro cep.")
-        $('#errorCep').removeClass('d-none')
-        $('#endereco').val("")
-    }else{
-       endereco = `${logradouro}${bairro}${cidade} - ${estado}`
-       $('#endereco').val(endereco)       
-       $('#errorCep').addClass('d-none')
-    }           
-}
-
-const spinnerCep = (botao) => {
-    setTimeout(function(){
-        $(botao).find('i').removeClass('d-none')
-        $(botao).find('span').addClass('d-none')
-    }, 1000) 
-}
